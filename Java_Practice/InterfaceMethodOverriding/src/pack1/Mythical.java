02e4927f961c3e11d23f77b6b2f88385fda47b7c
Jitendra Pisal <jitendra.pisal@forgeahead.io>
2017-01-12 17:26:27 +0530

    added java practice folder

---
+++ Java_Practice/InterfaceMethodOverriding/src/pack1/Mythical.java
@@ -0,0 +1,8 @@
+package pack1;
+
+public interface Mythical {
+	default public String identifyMyself() {
+        return "I am a mythical creature.";
+    }
+
+}
---
+++ Java_Practice/InterfaceMethodOverriding/src/pack1/Pegasus.java
@@ -0,0 +1,14 @@
+package pack1;
+
+public class Pegasus extends Horse implements Flyer,Mythical{
+	
+	public static void main(String[] args) {
+		
+		// Interface method overriding
+		Pegasus myApp = new Pegasus();
+		System.out.println(myApp.identifyMyself());
+		//*********************************************************************
+		
+		//
+	}
+}
